10c87b8d88a174d5318d8c8420f6200682cc24f5
Lincoln Ruteski <lruteski@gmail.com>
2022-05-11 12:01:22 -0300

    criando barra de vida, item e animações

---
+++ Assets/Scripts/BarraVida.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarraVida : MonoBehaviour
+{
+    public float vida; // valor entre 0 - 100
+    public Image barra;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        barra.fillAmount = vida / 100;
+    }
+}
